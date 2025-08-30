73af6c77bb2fdac08429c405a952d5209257df42
Melika7013 <payandehmelika62@gmail.com>
2025-08-30 16:19:23 +0330

    testing

--- Module3/task01.py
+++
@@ -1,6 +0,0 @@
-length = float(input("Enter the length of the zander in centimeters: "))
-if length >= 42:
-    print("The zander meets the size limit. You can keep it.")
-else:
-    print(f"The zander is too small! Release it back into the lake.")
-    print(f"It is {42 - length:.1f} cm below the size limit.")
